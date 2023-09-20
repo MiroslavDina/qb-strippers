d67dc4019e32e11f6beaa44d2eddd61838b87490
Watermellon86 <91654341+MiroslavDina@users.noreply.github.com>
2023-09-20 11:44:43 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config = {}
+
+Config.Locations18 = { -- Unicorn Girl tubo
+	--{ x = 109.05, y = -1283.27, z = 27.75, heading = 212.67 }
+	{ x = 118.84, y = -1302.44, z = 28.80, heading = 212.67 }
+}
+
+Config.Locations19 = { -- Unicorn Girl tubo
+	{ x = 107.9, y = -1290.02, z = 28.5, heading = 251.5 }
+}
+
+Config.Locations20 = { -- Unicorn Girl tubo
+	{ x = 99.93, y = -1287.55, z = 28.27, heading = 275.89 }
+}
+
+Config.Locations21 = { -- Unicorn Girl tubo
+	{ x = 105.96, y = -1297.26, z = 28.26, heading = 309.89 }
+}
+
+Config.Locations22 = { -- PolDancer 1
+	{ x = 102.22, y = -1290.68, z = 29.30, heading = 292.55 }
+}
+
+Config.Locations23 = { -- PolDancer 2
+	{ x = 104.25, y = -1293.98, z = 29.39, heading = 245.61 }
+}
+
+Config.Locations25 = { -- PolDancer 3
+	{ x = 112.68, y = -1286.74, z = 28.48, heading = 294.16 }
+}
+
+Config.Locations24 = { -- Unicorn Girls 
+	{ x = 116.86, y = -1292.91, z = 27.75, heading = 9.04 }
+}
+
